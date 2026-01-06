ba30b46c48377496c29e709b6d8f0c259beeb2c0
Constant09 <constantin.boubennec@eleves.ecmorlaix.fr>
2026-01-06 17:04:08 +0100

    Add files via upload

---
+++ facade.py
@@ -0,0 +1,24 @@
+from ma_rue import rue, affiche 
+from rectangle import rectangle
+
+# Fonction
+def facade(x, couleur, niveau):
+    '''
+    Dessine un rectangle de 60 pixels de haut et 140 pixels de large
+    Paramètres :
+        x : abcisse du centre de la façade
+        couleur : couleur de la façade fixée par l'immeuble
+        niveau : numéro du niveau (0 pour les rdc, ...)
+    '''
+    
+    y = rue.height - niveau * 60 # ordonnée de la base de la facade
+
+    rectangle(x,y,140,60,couleur)
+
+#Test
+if __name__ == '__main__':
+    from couleur_aleatoire import couleur_aleatoire
+    affiche(rue)
+    couleur = couleur_aleatoire()
+    for n in range(6) :
+        facade(rue.width/2, couleur, n)
